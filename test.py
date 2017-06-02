d8641ca9582f7a6e6a6e4d562f50e79cfbbe37f9
Dan Gaston <daniel.gaston@gmail.com>
2017-06-02 13:44:33 -0300

    Testing

--- test.py
+++ test.py
@@ -81,12 +81,12 @@ def run_bwa_mem(job, config, name, samples):
     with open(logfile, "wb") as err:
         sys.stdout.write("Executing {} and writing to logfile {}\n".format(command, logfile))
         err.write("Command: {}\n".format(command))
-        p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
-        output = p.communicate()
-        code = p.returncode
-        if code:
-            raise RuntimeError("An error occurred when executing the commandline: {}. "
-                               "Please check the logfile {} for details\n".format(command, logfile))
+        # p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
+        # output = p.communicate()
+        # code = p.returncode
+        # if code:
+        #     raise RuntimeError("An error occurred when executing the commandline: {}. "
+        #                        "Please check the logfile {} for details\n".format(command, logfile))
 
     return output_bam
 
