2e634ca9d7357776bb0b5d236ce100e4b588f17a
Dan Gaston <daniel.gaston@gmail.com>
2017-06-02 13:41:21 -0300

    Testing

--- test.py
+++ test.py
@@ -25,30 +25,6 @@ def create_file(job, fpath):
     # job.fileStore.writeGlobalFile(fpath)
 
 
-def run_and_log_command(command, logfile):
-    """This function uses the python subprocess method to run the specified command and writes all error to the
-    specified logfile
-
-    :param command: The command-line command to execute.
-    :type name: str.
-    :param logfile: The logfile to output error messages to.
-    :type logfile: str.
-    :returns:  Nothing
-    :raises: RuntimeError
-
-    """
-
-    with open(logfile, "wb") as err:
-        sys.stdout.write("Executing {} and writing to logfile {}\n".format(command, logfile))
-        err.write("Command: {}\n".format(command))
-        p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
-        output = p.communicate()
-        code = p.returncode
-        if code:
-            raise RuntimeError("An error occurred when executing the commandline: {}. "
-                               "Please check the logfile {} for details\n".format(command, logfile))
-
-
 def run_bwa_mem(job, config, name, samples):
     """Run GATK's DiagnoseTargets against the supplied region
 
@@ -101,7 +77,16 @@ def run_bwa_mem(job, config, name, samples):
     command = "{} | {} | {}".format(" ".join(bwa_cmd), " ".join(view_cmd), " ".join(sort_cmd))
 
     job.fileStore.logToMaster("BWA Command: {}\n".format(command))
-    run_and_log_command(command, logfile)
+
+    with open(logfile, "wb") as err:
+        sys.stdout.write("Executing {} and writing to logfile {}\n".format(command, logfile))
+        err.write("Command: {}\n".format(command))
+        p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
+        output = p.communicate()
+        code = p.returncode
+        if code:
+            raise RuntimeError("An error occurred when executing the commandline: {}. "
+                               "Please check the logfile {} for details\n".format(command, logfile))
 
     return output_bam
 
