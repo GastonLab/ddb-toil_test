458710ae15d6a8a30bdccbb61a053c6385c7f53f
Dan Gaston <daniel.gaston@gmail.com>
2017-06-02 13:45:45 -0300

    Testing

--- test.py
+++ test.py
@@ -78,15 +78,7 @@ def run_bwa_mem(job, config, name, samples):
 
     job.fileStore.logToMaster("BWA Command: {}\n".format(command))
 
-    with open(logfile, "wb") as err:
-        sys.stdout.write("Executing {} and writing to logfile {}\n".format(command, logfile))
-        err.write("Command: {}\n".format(command))
-        # p = sub.Popen(command, stdout=sub.PIPE, stderr=err, shell=True)
-        # output = p.communicate()
-        # code = p.returncode
-        # if code:
-        #     raise RuntimeError("An error occurred when executing the commandline: {}. "
-        #                        "Please check the logfile {} for details\n".format(command, logfile))
+    sub.call([command])
 
     return output_bam
 
